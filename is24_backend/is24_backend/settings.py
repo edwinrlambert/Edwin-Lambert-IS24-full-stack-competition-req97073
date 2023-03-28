38134def93bc8d6bde9deac3b83e51bb95503c9c
Edwin Lambert <edwinrlambert@gmail.com>
2023-03-27 19:40:12 -0700

    Setting up Django backend application.

--- is24_backend/is24_backend/settings.py
+++ is24_backend/is24_backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    
+    # Custom django-created apps.
+    "api.apps.ApiConfig",
 ]
 
 MIDDLEWARE = [
