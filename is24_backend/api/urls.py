60595385885fbae12a98ae4ca0aa5844a40b7b98
Edwin Lambert <edwinrlambert@gmail.com>
2023-03-27 20:50:45 -0700

    Wrote function to read json in backend.

--- is24_backend/api/urls.py
+++ is24_backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import main, read_json_file
 
 urlpatterns = [
-    path("", main)
+    # path("", main),
+    path("", read_json_file)
 ]
